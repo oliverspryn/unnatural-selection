d4bd8a2ae4c63f167a67e8b4d711daa236cc55a2
michaelhfl <wiegandma1@gcc.edu>
2013-11-05 16:41:24 -0500

    Created a WeaponsTest Game.
    This will help in the testing of guns when there being made

--- UnnaturalSelection/weaponTest.cpp
+++ UnnaturalSelection/weaponTest.cpp
@@ -3,4 +3,190 @@
 // 11/5/2013
 
 #include "weaponTest.h"
+#include <sstream>
 
+//=============================================================================
+// Constructor
+//=============================================================================
+WeaponTest::WeaponTest()
+{
+    menuOn = true;
+	timer = 0;
+	frameCount = 0;
+	testGun = 0;
+	testMag = 0;
+	testProjectile = 0;
+    initialized = false;
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+WeaponTest::~WeaponTest()
+{
+    releaseAll();           // call onLostDevice() for every graphics item
+	if(testGun > 0)
+	{
+		delete testGun;
+	}
+	if(testMag > 0)
+	{
+		delete testMag;
+	}
+	if(testProjectile > 0)
+	{
+		delete testProjectile;
+	}
+}
+
+//=============================================================================
+// Initializes the game
+// Throws GameError on error
+//=============================================================================
+void WeaponTest::initialize(HWND hwnd)
+{
+    Game::initialize(hwnd); // throws GameError
+
+    // Gun texture
+    if (!gunTM.initialize(graphics,"pictures\\AssultRifle(128x32).png"))
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing gun texture"));
+
+    // Weapon texture
+    if (!magTM.initialize(graphics,"pictures\\mag(8x8).png"))
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing mag texture"));
+
+    // Projectile textures
+    if (!projectileTM.initialize(graphics,"pictures\\projectile(8x8).png"))
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing projectile textures"));
+
+    if (!gunIM.initialize(graphics,0,0,0,&gunTM))
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing gun"));
+
+    if (!magIM.initialize(graphics,0,0,0,&magTM))
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing mag"));
+
+	if (!projectileIM.initialize(graphics,0,0,0,&projectileTM))
+        throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing projectile"));
+
+    return;
+}
+
+//=============================================================================
+// Update all game items
+//=============================================================================
+void WeaponTest::update()
+{
+	timer += frameTime;
+	frameCount += 1;
+    if (menuOn)
+    {
+        if (input->anyKeyPressed())
+        {
+            menuOn = false;
+            input->clearAll();
+            roundStart();
+        }
+    } 
+    else 
+    {
+        
+    }
+}
+
+//=============================================================================
+// Start a new round of play
+//=============================================================================
+void WeaponTest::roundStart()
+{
+    // Start ships on opposite sides of Star in stable clockwise orbit
+   
+}
+
+//=============================================================================
+// Artificial Intelligence
+//=============================================================================
+void WeaponTest::ai()
+{
+
+}
+
+//=============================================================================
+// Handle collisions
+//=============================================================================
+void WeaponTest::collisions()
+{
+    VECTOR2 collisionVector;
+    
+}
+
+//=============================================================================
+// Render game items
+//=============================================================================
+void WeaponTest::render()
+{
+    graphics->spriteBegin();                // begin drawing sprites
+
+	graphics->setBackColor(graphicsNS::CYAN);
+
+    graphics->spriteEnd();                  // end drawing sprites
+}
+
+//=============================================================================
+// process console commands
+//=============================================================================
+void WeaponTest::consoleCommand()
+{
+    command = console->getCommand();    // get command from console
+    if(command == "")                   // if no command
+        return;
+
+    if (command == "help")              // if "help" command
+    {
+        console->print("Console Commands:");
+        console->print("fps");
+        console->print("afps");
+        return;
+    }
+    if (command == "fps")
+    {
+		std::stringstream temp;
+		temp << "FPS: " << 1.0/frameTime;
+		console->print(temp.str());
+		return;
+    }
+	if (command == "afps")
+    {
+		std::stringstream temp;
+		temp << "Average FPS: " << 1.0/(timer/frameCount);
+		console->print(temp.str());
+		return;
+    }
+}
+
+//=============================================================================
+// The graphics device was lost.
+// Release all reserved video memory so graphics device may be reset.
+//=============================================================================
+void WeaponTest::releaseAll()
+{
+    gunTM.onLostDevice();
+    magTM.onLostDevice();
+    projectileTM.onLostDevice();
+
+    Game::releaseAll();
+    return;
+}
+
+//=============================================================================
+// The grahics device has been reset.
+// Recreate all surfaces.
+//=============================================================================
+void WeaponTest::resetAll()
+{
+    gunTM.onResetDevice();
+    magTM.onResetDevice();
+    projectileTM.onResetDevice();
+
+    Game::resetAll();
+    return;
+}
--- UnnaturalSelection/weaponTest.h
+++ UnnaturalSelection/weaponTest.h
@@ -5,12 +5,58 @@
 #ifndef WEAPONTEST_H
 #define WEAPONTEST_H
 #define WIN32_LEAN_AND_MEAN
+
+#include <string>
 #include "game.h"
+#include "textureManager.h"
+#include "image.h"
+#include "dashboard.h"
+#include "gun.h"
+
+
+namespace weapontestNS
+{
+	const int BUFFER_SIZE = 20;
+}
+
 
 class WeaponTest: public Game
 {
+public:
+	/*********************************************
+	ALL VARIBLES
+	**********************************************/
+	TextureManager gunTM, magTM, projectileTM;   // textures
+	Image   gunIM;
+    Image   magIM;
+    Image   projectileIM;
+    Gun* testGun;
+	Magazine* testMag;
+	Projectile* testProjectile;
+    bool    menuOn;
+	char buffer[weapontestNS::BUFFER_SIZE];
+    float   timer;         // time until new round starts
+	int frameCount;
 
 
+	/*********************************************
+	ALL FUNCTIONS
+	**********************************************/
+
+	// Constructor
+    WeaponTest();
+    // Destructor
+    virtual ~WeaponTest();
+    // Initialize the game
+    void initialize(HWND hwnd);
+    void update();      // must override pure virtual from Game
+    void ai();          // "
+    void collisions();  // "
+    void render();      // "
+    void consoleCommand(); // process console command
+    void roundStart();  // start a new round of play
+    void releaseAll();
+    void resetAll();
 };
 
 #endif
