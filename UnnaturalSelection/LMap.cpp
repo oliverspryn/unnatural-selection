5aac6d70d4390037d474e3f117cc7e71266faa3c
michaelhfl <wiegandma1@gcc.edu>
2013-11-17 23:50:00 -0500

    Target Kill update
    Boooyah Kahsha

--- UnnaturalSelection/LMap.cpp
+++ UnnaturalSelection/LMap.cpp
@@ -200,12 +200,18 @@ void LMap::update(float frameTime)
 				}
 			}
 			float tempTime = frameTime;
-			for(int k(0); k < totalTargets && targets[k] != 0; k++)
+			for(int k(0); k < totalTargets && (targets[k] != 0 && targets[k]->getActive()); k++)
 			{
 				if(projectileCollide(*mags[i]->projArray[j], *targets[k], tempTime))
 				{
 					mags[i]->projArray[j]->setActive(false);
 					mags[i]->projArray[j]->setVisible(false);
+					targets[k]->setHealth(targets[k]->getHealth() - mags[i]->projArray[j]->damage);
+					if(targets[k]->getHealth() < 0)
+					{
+						targets[k]->setActive(false);
+						targets[k]->setVisible(false);
+					}
 				}
 			}
 		}
