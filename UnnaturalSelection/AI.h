d358c222971d73dd9b61e6c4e4b805e2f7a40a3d
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2013-12-07 22:32:07 -0500

    AI is here. And I don't mean Albert.

---
+++ UnnaturalSelection/AI.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "C:\Users\sprynoj1\Documents\Visual Studio 2013\Projects\unnatural-selection\source\unnatural-selection\UnnaturalSelection\Character.h"
+class AI :
+	public Character
+{
+public:
+	AI();
+	~AI();
+};
+
