e1e2cdc9e69060a39f3c0016b440a2c4c7d6ee2f
michaelhfl <wiegandma1@gcc.edu>
2013-12-08 15:36:30 -0500

    New sprite

--- UnnaturalSelection/Body.h
+++ UnnaturalSelection/Body.h
@@ -9,15 +9,15 @@
 
 namespace bodyNS {
 //Basic config
-	const float ANIMATION_DELAY    = 0.2f;
-	const int   COLS               = 4;
+	const float ANIMATION_DELAY    = 0.1f;
+	const int   COLS               = 8;
 	const int   CROUCH_HEIGHT      = 34;
 	const int   FACE_DIRECTION     = 1;  // 1 = right, 0 | -1 = left
-	const char  GRAPHIC[]          = "pictures\\body.png";
-	const int   HEIGHT             = 66; // Height + frame
+	const char  GRAPHIC[]          = "pictures\\SpriteSheet2.png";
+	const int   HEIGHT             = 128; // Height + frame
 	const int   START_FRAME        = 0;  // Must face in the the direction of FACE_DIRECTION
 	const int   STOP_FRAME         = 3;
-	const int   WIDTH              = 97; // Width + frame
+	const int   WIDTH              = 128; // Width + frame
 
 //Crouch
 	const int   CROUCH_LEFT_START  = 0;
@@ -26,34 +26,34 @@ namespace bodyNS {
 	const int   CROUCH_RIGHT_STOP  = 7;
 
 //Fall
-	const int   FALL_LEFT_START    = 16;
-	const int   FALL_LEFT_STOP     = 19;
-	const int   FALL_RIGHT_START   = 20;
-	const int   FALL_RIGHT_STOP    = 23;
+	const int   FALL_LEFT_START    = 3;
+	const int   FALL_LEFT_STOP     = 3;
+	const int   FALL_RIGHT_START   = 11;
+	const int   FALL_RIGHT_STOP    = 11;
 
 //Jump
-	const int   JUMP_LEFT_START    = 16;
-	const int   JUMP_LEFT_STOP     = 19;
-	const int   JUMP_RIGHT_START   = 20;
-	const int   JUMP_RIGHT_STOP    = 23;
+	const int   JUMP_LEFT_START    = 3;
+	const int   JUMP_LEFT_STOP     = 3;
+	const int   JUMP_RIGHT_START   = 11;
+	const int   JUMP_RIGHT_STOP    = 11;
 
 //Idle
-	const int   LOOK_LEFT_START    = 0;
-	const int   LOOK_LEFT_STOP     = 3;
-	const int   LOOK_RIGHT_START   = 4;
-	const int   LOOK_RIGHT_STOP    = 7;
+	const int   LOOK_LEFT_START    = 24;
+	const int   LOOK_LEFT_STOP     = 24;
+	const int   LOOK_RIGHT_START   = 16;
+	const int   LOOK_RIGHT_STOP    = 16;
 
 //Run
-	const int   RUN_LEFT_START     = 0;
-	const int   RUN_LEFT_STOP      = 3;
-	const int   RUN_RIGHT_START    = 4;
-	const int   RUN_RIGHT_STOP     = 7;
+	const int   RUN_LEFT_START     = 16;
+	const int   RUN_LEFT_STOP      = 16;
+	const int   RUN_RIGHT_START    = 16;
+	const int   RUN_RIGHT_STOP     = 16;
 
 //Walk
 	const int   WALK_LEFT_START    = 8;
-	const int   WALK_LEFT_STOP     = 11;
-	const int   WALK_RIGHT_START   = 12;
-	const int   WALK_RIGHT_STOP    = 15;
+	const int   WALK_LEFT_STOP     = 13;
+	const int   WALK_RIGHT_START   = 0;
+	const int   WALK_RIGHT_STOP    = 5;
 }
 
 class Body : public Entity {
