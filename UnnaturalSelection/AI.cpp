df717dfd0ae07f28773dbd1a0e322c15f4ed2d55
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2013-12-07 23:24:56 -0500

    AI (Albert) fixes

--- UnnaturalSelection/AI.cpp
+++ UnnaturalSelection/AI.cpp
@@ -1,11 +1,9 @@
 #include "AI.h"
 
+AI::AI(Game* game, Graphics* graphics) : Character(game, graphics) {
 
-AI::AI()
-{
 }
 
+AI::~AI() {
 
-AI::~AI()
-{
 }
--- UnnaturalSelection/AI.h
+++ UnnaturalSelection/AI.h
@@ -1,10 +1,14 @@
-#pragma once
-#include "C:\Users\sprynoj1\Documents\Visual Studio 2013\Projects\unnatural-selection\source\unnatural-selection\UnnaturalSelection\Character.h"
-class AI :
-	public Character
-{
-public:
-	AI();
+#ifndef AI_H
+#define AI_H
+
+#include "Character.h"
+#include "game.h"
+#include "graphics.h"
+
+class AI : public Character {
+public : 
+	AI(Game* game, Graphics* graphics);
 	~AI();
 };
 
+#endif
